5aae341d27560558a85dba49e86994cb66996a3c
luthfidiqi <luthfidiqi@gmail.com>
2023-08-05 16:33:29 +0700

    update

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  socketPath: process.env.MYSQL_SOCKETPATH,
+  // socketPath: process.env.MYSQL_SOCKETPATH,
 });
 
 connection.connect((error) => {
